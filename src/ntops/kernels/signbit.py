9d2db72f757964a7f89c5195d4e7017190cf07b0
xuzhengzhong <xuzhzhong@163.com>
2026-02-27 20:30:28 +0800

    Update  operator

--- src/ntops/kernels/signbit.py
+++ src/ntops/kernels/signbit.py
@@ -1,21 +1,20 @@
 import functools
 
 from ninetoothed import Tensor
+import ninetoothed.language as ntl
 
 from ntops.kernels.element_wise import arrangement
 
-
 def application(input, output):
-    # signbit(x) should be True for:
-    # 1) x < 0
-    # 2) x is -0.0 (important: -0.0 has sign bit set but x < 0 is False)
-    neg = input < 0
-
-    is_zero = input == 0
-    neg_zero = is_zero & ((1 / input) == float("-inf"))
-
-    output = neg | neg_zero  # noqa: F841
-
+    if input.dtype is ntl.float16:
+        i_unint = ntl.cast(input, ntl.uint16, bitcast=True)
+        output = (i_unint >> 15) & 0x1  # noqa: F841
+    elif input.dtype is ntl.float32:
+        i_unint = ntl.cast(input, ntl.uint32, bitcast=True)
+        output = (i_unint >> 31) & 0x1  # noqa: F841
+    elif input.dtype is ntl.float64:
+        i_unint = ntl.cast(input, ntl.uint64, bitcast=True)
+        output = (i_unint >> 63) & 0x1  # noqa: F841
 
 def premake(ndim, dtype=None, block_size=None):
     arrangement_ = functools.partial(arrangement, block_size=block_size)
@@ -23,3 +22,20 @@ def premake(ndim, dtype=None, block_size=None):
     tensors = (Tensor(ndim, dtype=dtype), Tensor(ndim, dtype=dtype))
 
     return arrangement_, application, tensors
+
+# import torch
+# dtype = torch.float32
+# device = "cuda"
+
+# input = torch.tensor([-1.0, 0.0, 1.0], dtype=dtype, device=device)
+# ref = torch.signbit(input)
+# output = torch.empty_like(ref)
+
+# from ntops.torch.utils import _cached_make
+# kernel = _cached_make(premake, input.dim())
+
+# kernel(input, output)
+
+# print("Input:", input)
+# print("Output:", output)
+# print("Reference:", ref)
--- src/ntops/torch/signbit.py
+++ src/ntops/torch/signbit.py
@@ -6,8 +6,8 @@ from ntops.torch.utils import _cached_make
 
 def signbit(input, *, out=None):
     if out is None:
-        out = torch.empty_like(input)
-
+        out = torch.empty_like(input, dtype=torch.bool)
+    
     kernel = _cached_make(ntops.kernels.signbit.premake, input.ndim)
 
     kernel(input, out)
