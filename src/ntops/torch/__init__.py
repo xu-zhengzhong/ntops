97e32090df847db75f1dc48372b57594517b429d
xuzhengzhong <xuzhzhong@163.com>
2026-08-31 00:46:08 +0800

    Apply Ruff fixes to `__init__.py` files

--- src/ntops/torch/__init__.py
+++ src/ntops/torch/__init__.py
@@ -1,8 +1,16 @@
 from ntops.torch.abs import abs
+from ntops.torch.acosh import acosh
+from ntops.torch.adaptive_avg_pool2d import adaptive_avg_pool2d
+from ntops.torch.adaptive_max_pool2d import adaptive_max_pool2d
 from ntops.torch.add import add
 from ntops.torch.addmm import addmm
+from ntops.torch.addmv import addmv
 from ntops.torch.alpha_dropout import alpha_dropout
+from ntops.torch.argsort import argsort
+from ntops.torch.atan import atan
 from ntops.torch.avg_pool2d import avg_pool2d
+from ntops.torch.batch_norm import batch_norm
+from ntops.torch.bincount import bincount
 from ntops.torch.bitwise_and import bitwise_and
 from ntops.torch.bitwise_not import bitwise_not
 from ntops.torch.bitwise_or import bitwise_or
@@ -17,6 +25,7 @@ from ntops.torch.div import div
 from ntops.torch.dropout import dropout
 from ntops.torch.eq import eq
 from ntops.torch.exp import exp
+from ntops.torch.fmax import fmax
 from ntops.torch.ge import ge
 from ntops.torch.gelu import gelu
 from ntops.torch.gt import gt
@@ -25,9 +34,19 @@ from ntops.torch.isinf import isinf
 from ntops.torch.isnan import isnan
 from ntops.torch.layer_norm import layer_norm
 from ntops.torch.le import le
+from ntops.torch.logsumexp import logsumexp
+from ntops.torch.lp_pool1d import lp_pool1d
+from ntops.torch.lp_pool2d import lp_pool2d
+from ntops.torch.lp_pool3d import lp_pool3d
 from ntops.torch.lt import lt
 from ntops.torch.matmul import matmul
+from ntops.torch.max import max
+from ntops.torch.max_pool1d import max_pool1d
 from ntops.torch.max_pool2d import max_pool2d
+from ntops.torch.max_pool3d import max_pool3d
+from ntops.torch.maximum import maximum
+from ntops.torch.mean import mean
+from ntops.torch.median import median
 from ntops.torch.mm import mm
 from ntops.torch.msort import msort
 from ntops.torch.mul import mul
@@ -51,29 +70,10 @@ from ntops.torch.silu import silu
 from ntops.torch.sin import sin
 from ntops.torch.softmax import softmax
 from ntops.torch.sort import sort
+from ntops.torch.stack import stack
 from ntops.torch.sub import sub
 from ntops.torch.tanh import tanh
 from ntops.torch.threshold import threshold
-from ntops.torch.max_pool1d import max_pool1d
-from ntops.torch.max_pool3d import max_pool3d
-from ntops.torch.stack import stack
-from ntops.torch.mean import mean
-from ntops.torch.median import median
-from ntops.torch.maximum import maximum
-from ntops.torch.atan import atan
-from ntops.torch.batch_norm import batch_norm
-from ntops.torch.bincount import bincount
-from ntops.torch.adaptive_max_pool2d import adaptive_max_pool2d
-from ntops.torch.acosh import acosh
-from ntops.torch.adaptive_avg_pool2d import adaptive_avg_pool2d
-from ntops.torch.addmv import addmv
-from ntops.torch.argsort import argsort
-from ntops.torch.fmax import fmax
-from ntops.torch.logsumexp import logsumexp
-from ntops.torch.lp_pool1d import lp_pool1d
-from ntops.torch.lp_pool2d import lp_pool2d
-from ntops.torch.lp_pool3d import lp_pool3d
-from ntops.torch.max import max
 
 __all__ = [
     "abs",
@@ -130,7 +130,7 @@ __all__ = [
     "softmax",
     "sort",
     "sub",
-    "tanh",    
+    "tanh",
     "max_pool1d",
     "max_pool3d",
     "stack",
